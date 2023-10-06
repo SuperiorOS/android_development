Madeinmisery/Keyboard

Map cargo2android.json dep-suffixes to module_name_overrides.
Bug: 293289578
Test: Ran on anes.
Change-Id: I1a256ebd0ddf670ef0aaa9e3e2989fd6b96a37a2

tools/cargo_embargo/src/config/legacy.rs
@@ -29,6 +29,8 @@ pub struct Config {
     apex_available: Vec<String>,
     #[serde(default)]
     cfg_blocklist: Vec<String>,
+    #[serde(default)]
+    dep_suffixes: BTreeMap<String, String>,
     #[allow(unused)] // Deprecated option.
     #[serde(default)]
     dependencies: bool,
@@ -87,6 +89,15 @@ impl Config {
             .iter()
             .map(|test_filename| test_filename_to_module_name(package_name, test_filename))
             .collect();
+        let module_name_overrides = self
+            .dep_suffixes
+            .iter()
+            .map(|(dependency, suffix)| {
+                let package_name = package_to_library_name(dependency);
+                let with_suffix = format!("{}{}", package_name, suffix);
+                (package_name, with_suffix)
+            })
+            .collect();
         let package_config = PackageConfig {
             device_supported: self.device,
             host_supported: !self.no_host,
@@ -115,6 +126,7 @@ impl Config {
             vendor_available: self.vendor_available,
             min_sdk_version: self.min_sdk_version.clone(),
             module_blocklist,
+            module_name_overrides,
             package,
             run_cargo,
             ..Default::default()

SuperiorOS/android_development

tools/cargo_embargo/src/config/legacy.rs
@@ -29,6 +29,8 @@ pub struct Config {
     apex_available: Vec<String>,
     #[serde(default)]
     cfg_blocklist: Vec<String>,
+    #[serde(default)]
+    dep_suffixes: BTreeMap<String, String>,
     #[allow(unused)] // Deprecated option.
     #[serde(default)]
     dependencies: bool,
@@ -89,6 +91,15 @@ impl Config {
             .iter()
             .map(|test_filename| test_filename_to_module_name(package_name, test_filename))
             .collect();
+        let module_name_overrides = self
+            .dep_suffixes
+            .iter()
+            .map(|(dependency, suffix)| {
+                let module_name = package_to_library_name(dependency);
+                let with_suffix = format!("{}{}", module_name, suffix);
+                (module_name, with_suffix)
+            })
+            .collect();
         let package_config = PackageConfig {
             device_supported: self.device,
             force_rlib: self.force_rlib,
@@ -118,6 +129,7 @@ impl Config {
             vendor_available: self.vendor_available,
             min_sdk_version: self.min_sdk_version.clone(),
             module_blocklist,
+            module_name_overrides,
             package,
             run_cargo,
             ..Default::default()